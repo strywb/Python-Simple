d0951a18f03025aabdb7b86a13fecba81b764cae
DulanaSenavirathna <dulana.insfra@gmail.com>
2021-10-09 20:14:24 +0530

    arrays in python

---
+++ arrays.py
@@ -0,0 +1,8 @@
+#Arrays are used to store multiple values in one single variable.
+SimpleArray = ["Item1", "Item2", "Item3"]
+
+#output array
+print(SimpleArray)
+
+#we can access an array element by referring to the index number
+print(SimpleArray[0])
