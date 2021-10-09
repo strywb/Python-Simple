81a4376b3eca0c0722317cb2107b5ef99dea0e87
DulanaSenavirathna <dulana.insfra@gmail.com>
2021-10-09 21:42:04 +0530

    variables in python

---
+++ variables.py
@@ -0,0 +1,11 @@
+a = 10
+b = "Simple Python"
+print(a)
+print(b)
+
+#Variables don't need to be declared with any particular type, 
+#and this can even change type after they have been assign early.
+x = 4
+print(x)
+x = "Variable Type changed"
+print(x)
